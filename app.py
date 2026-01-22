238e905ff8747caa1c42fa49d14dc331970aa00b
William Gaultier <77394825+wgaulti@users.noreply.github.com>
2026-01-22 10:39:57 +0100

    add folder name parsing to add to config

---
+++ app.py
@@ -0,0 +1,20 @@
+import multiprocessing
+import sys
+
+# Required for PyInstaller to handle multiprocessing correctly
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+
+import customtkinter as ctk
+from ui.app_view import AppView
+
+ctk.set_appearance_mode("system")      # "light" | "dark" | "system"
+ctk.set_default_color_theme("blue")    # "blue" | "green" | "dark-blue"
+ctk.deactivate_automatic_dpi_awareness()  # prevent alpha flicker/opacity when moving between monitors
+
+def main():
+    app = AppView()
+    app.mainloop()
+
+if __name__ == "__main__":
+    main()
